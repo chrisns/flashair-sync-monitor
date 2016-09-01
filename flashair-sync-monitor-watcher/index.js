5c7f18f91d7864f45ccde462930a914db77db3cf
Chris Nesbitt-Smith <chris@cns.me.uk>
2016-09-01 22:11:53 +0100

    flashair-watcher

---
+++ flashair-sync-monitor-watcher/index.js
@@ -0,0 +1,15 @@
+const flashair = require("flashair")(process.env.FLASH_AIR_IP, "AP");
+
+flashair.command.getUpdateStatus(status => {
+  if (status) {
+    flashair.command.getFileList("/", (err, files) => {
+      if (err) {
+        throw err;
+      }
+      console.log(files);
+      //  get files that we don't already have then delete the file
+    });
+  }
+})
+
+
